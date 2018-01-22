6d8178d2cef0d3621693f892a9ee1fa500436989
danny <dpena1094@users.noreply.github.com>
2018-01-22 13:29:16 -0800

    Changes

--- script_test.py
+++ script_test.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-print('sup bruh')
+def main():
+    print('sup bruh')
+
+if __name__ == '__main__':
+    main()
