00509912040d257346a839da2a44e23c6742e544
danny <dpena1094@users.noreply.github.com>
2018-01-29 10:30:32 -0800

    Changes, again

--- socket_test.py
+++ socket_test.py
@@ -3,13 +3,20 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from socketIO_client import BaseNamespace
 from socketIO_client import LoggingNamespace
 from socketIO_client import SocketIO
 
 def test(*args):
     print(args)
 
+class Namespace(BaseNamespace):
+    def on_tester(self, *args):
+        print('test: ', args)
+
 socketio = SocketIO('http://ec2-52-24-126-225.us-west-2.compute.amazonaws.com', 81, LoggingNamespace)
-socketio.on('tester', test)
-socketio.emit('testee', 'asdf')
+io = socketio.define(Namespace, '/socket.io')
+
+io.emit('testee', 'hello')
 socketio.wait(seconds=1)
+
