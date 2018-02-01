33fd7d1b36448e176efa327ce7a61c2d8a5d9028
danny <dpena1094@users.noreply.github.com>
2018-01-31 20:11:08 -0800

    Changes

--- socket_test.py
+++ socket_test.py
@@ -11,12 +11,16 @@ def test(*args):
     print(args)
 
 class Namespace(BaseNamespace):
-    def on_tester(self, *args):
-        print('test: ', args)
+	def on_tester(self, *args):
+		print('test: ', args)
+
+	def on_cm_on_mission_initialized(self, *args):
+        	print('test: ', args)
 
 socketio = SocketIO('http://ec2-52-24-126-225.us-west-2.compute.amazonaws.com', 81, LoggingNamespace)
 io = socketio.define(Namespace, '/socket.io')
 
 io.emit('testee', 'hello')
-socketio.wait(seconds=1)
+while True:
+	socketio.wait(seconds=1)
 
